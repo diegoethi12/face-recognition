881d5d2a5f8fc3da51efb2d298d61f3cc0eb2494
Lopez <dlopez@analytics.cl>
2018-10-19 16:45:23 -0300

    Finalizado extract-embeddings

--- extract-embeddings.py
+++ extract-embeddings.py
@@ -22,13 +22,12 @@ ap.add_argument("-c", "--confidence", type=float, default=0.5,
 args = vars(ap.parse_args())
 
 args = {}
-print(args)
-
 args["dataset"] = 'dataset'
-args["embeddings"] = ''
+args["embeddings"] = 'embeddings'
 args["detector"] = 'models\\face-detection'
 args["embedding_model"] = 'models\\embedding-model'
-args["confidence"] = 1
+args["confidence"] = 0.5
+print(args)
 
 # load our serialized face detector from disk
 print("[INFO] loading face detector...")
@@ -67,5 +66,72 @@ for (i, imagePath) in enumerate(imagePaths):
 	# maintaining the aspect ratio), and then grab the image
 	# dimensions
 	image = cv2.imread(imagePath)
+
 	image = imutils.resize(image, width=600)
 	(h, w) = image.shape[:2]
+
+		# construct a blob from the image
+	imageBlob = cv2.dnn.blobFromImage(
+		cv2.resize(image, (300, 300)), 1.0, (300, 300),
+		(104.0, 177.0, 123.0), swapRB=False, crop=False)
+ 
+	# apply OpenCV's deep learning-based face detector to localize
+	# faces in the input image
+	detector.setInput(imageBlob)
+	detections = detector.forward()
+
+	# ensure at least one face was found
+	if len(detections) > 0:
+		# we're making the assumption that each image has only ONE
+		# face, so find the bounding box with the largest probability
+		i = np.argmax(detections[0, 0, :, 2])
+		confidence = detections[0, 0, i, 2]
+ 
+		# ensure that the detection with the largest probability also
+		# means our minimum probability test (thus helping filter out
+		# weak detections)
+		if confidence > args["confidence"]:
+			# compute the (x, y)-coordinates of the bounding box for
+			# the face
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+ 
+			# extract the face ROI and grab the ROI dimensions
+			face = image[startY:endY, startX:endX]
+			(fH, fW) = face.shape[:2]
+
+			# ensure the face width and height are sufficiently large
+			if fW < 20 or fH < 20:
+				continue
+
+			# construct a blob for the face ROI, then pass the blob
+			# through our face embedding model to obtain the 128-d
+			# quantification of the face
+			faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255,
+				(96, 96), (0, 0, 0), swapRB=True, crop=False)
+			embedder.setInput(faceBlob)
+			vec = embedder.forward()
+
+			# add the name of the person + corresponding face
+			# embedding to their respective lists
+			knownNames.append(name)
+			knownEmbeddings.append(vec.flatten())
+			total += 1
+
+# dump the facial embeddings + names to disk
+print("[INFO] serializing {} encodings...".format(total))
+data = {"embeddings": knownEmbeddings, "names": knownNames}
+
+os.chdir(os.path.sep.join([os.getcwd(),'embeddings']))
+
+f = open(args["embeddings"], "wb")
+f.write(pickle.dumps(data))
+f.close()
+
+#while(True):
+#    cv2.imshow('hola',face)
+#    k = cv2.waitKey(0)
+#    print(k)
+#    if k == 27: #escape
+#        cv2.destroyAllWindows()
+#        break
